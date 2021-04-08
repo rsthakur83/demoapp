ad793521ed6e94224278357830ef279c1b93ecd8
rsthakur83 <rsthakur83@yahoo.com>
2021-04-08 17:58:38 +0200

    Update root.go

--- cmd/root.go
+++ cmd/root.go
@@ -50,7 +50,7 @@ var rootCmd = &cobra.Command{
 This application is used as part of challenging potential candiates at Sevian.
 
 Please visit http://Servian.com for more details`,
-  Version: "1.8.0-alpha1",
+  Version: "1.9.1",
 }
 
 // Execute adds all child commands to the root command and sets flags appropriately.
